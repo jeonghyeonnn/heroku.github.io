0654d541e9532513821c8b9a276af023b928fa76
김정현1 <jeonghyeonn@gimjeonghyeon-ui-MacBookAir.local>
2019-07-31 23:44:01 +0900

    first commit

--- first/urls.py
+++ first/urls.py
@@ -4,12 +4,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.new, name="new"),
+    path('', views.index, name="index"),
     path('create/', views.create, name='create'),
     path('update/<int:pk>', views.update, name ='update'),
     path('delete/<int:pk>', views.delete, name='delete'),
 
-    path('index/', views.index, name='index'),
+    path('new/', views.new, name='new'),
     path('comment/<int:post_pk>', views.comment, name='comment'),
     path('upda/<int:post_pk>',views.upda, name='upda'),
     path('dele/<int:post_pk>', views.dele, name='dele'),
